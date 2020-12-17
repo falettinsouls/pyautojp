c60dd35916e7fc302152beccd954e9e2d3e600ad
falettinsouls <74698969+falettinsouls@users.noreply.github.com>
2020-12-17 16:19:16 +0900

    Add files via upload
    WebサイトやKeynote/PowerPointなど、ポイント単位で画像を等間隔で配置する時のプログラム。ExcelでもできることをPythonで記述しました。
    a:初期位置； b:増分； counter: while繰り返しぶん としている。この3つを状況に応じて書き換えて用います。
    
    in English：
    This is a program for placing images at equal intervals in points on a website, Keynote, PowerPoint, etc. What can be done in Excel can be done in Python.
    # a: initial position; b: increment; counter: number of times of repeating using by "while" . These three valuables can be rewritten and used according to the your own situation.

---
+++ layout_calc.py
@@ -0,0 +1,15 @@
+a = 100
+b = 35
+counter = 12
+
+print(a)
+while counter > 0:
+	print(a+b)
+	a = (a+b)
+	counter = counter -1
+
+# WebサイトやKeynote/PowerPointなど、ポイント単位で画像を等間隔で配置する時のプログラム。ExcelでもできることをPythonで記述している。
+# a:初期位置； b:増分； counter: while繰り返しぶん としている。この3つを状況に応じて書き換えて用いる。
+
+# This is a program for placing images at equal intervals in points on a website, Keynote, PowerPoint, etc. What can be done in Excel can be done in Python.
+# a: initial position; b: increment; counter: number of times of repeating using by "while" . These three valuables can be rewritten and used according to the your own situation.
