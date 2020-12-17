158d8bf0a23d1c86949b4420312496f21a646896
falettinsouls <74698969+falettinsouls@users.noreply.github.com>
2020-12-17 16:38:31 +0900

    Add files via upload
    #in Japanese: これは 任意の日数を代入して実行するだけで、「今日からx日後が何年の何月何日かをタイムスタンプで表示させるプログラムです。 days の初期変数である「16」は好きに書き換えてください。
    
    #in English: This is a program that displays a timestamp of what day of the year is x days from today by simply substituting any number of days and running it. The initial variable of days, "16", can be rewritten as you like.

---
+++ x_days_after.py
@@ -0,0 +1,11 @@
+import datetime
+now = datetime.datetime.now()
+
+days = 16
+
+print (now)
+print (now + datetime.timedelta(days))
+
+#in Japanese: これは 任意の日数を代入して実行するだけで、「今日からx日後が何年の何月何日かをタイムスタンプで表示させるプログラムです。 days の初期変数である「16」は好きに書き換えてください。
+
+#in English: This is a program that displays a timestamp of what day of the year is x days from today by simply substituting any number of days and running it. The initial variable of days, "16", can be rewritten as you like.
